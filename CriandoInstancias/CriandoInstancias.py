65ec484599cda48aae0911472da715d4dde21b67
Ellyda Micaella <ellydamicaella810@gmail.com>
2024-10-18 02:12:22 -0300

    primeiro commit

---
+++ CriandoInstancias/CriandoInstancias.py
@@ -0,0 +1,8 @@
+''' 1 º passo criar chave key
+2 º passo criar a função Lambda
+        - cloudwatch
+        - Ec2
+3º script python -> variaveis
+        - t2.micro
+        - iam
+        - linux'''
